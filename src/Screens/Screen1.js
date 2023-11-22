2d97ab57488eb41ecb7f2039d2597c27875b52b8
Suhadmendis <suhad.a.mendis@gmail.com>
2023-11-23 02:04:02 +0530

    starter

--- src/Screens/Screen1.js
+++ src/Screens/Screen1.js
@@ -6,11 +6,7 @@ const cover = require("../assets/images/getstarted/cover.png");
 const Screen1 = () => {
   return (
     <View style={styles.container}>
-      <Image source={cover} resizeMode="contain" style={styles.cover} />
-      <View style={styles.imageArea}>
-        <Image source={book} resizeMode="contain" style={styles.image} />
-      </View>
-      <Text style={styles.mainText}>Book Share</Text>
+
     </View>
   );
 };
@@ -24,26 +20,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  imageArea: {
-    width: 60,
-    height: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "white",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-  },
-  mainText: {
-    color: "white",
-    marginTop: 15,
-    fontSize: 20,
-  },
-  cover: {
-    position: "absolute",
-    width: 200,
-    height: 950,
-    left: 35,
-  }
+
 });
--- src/Screens/Screen2.js
+++ src/Screens/Screen2.js
@@ -25,49 +25,11 @@ const data = [
 
 const Screen2 = () => {
 
-  const [selected, setSelected] = useState(data[0]);
 
-  const setData = (element) => {
-    setSelected(element);
-  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.topPanel}>
-        <TouchableOpacity activeOpacity={0.9}>
-          <Text style={styles.skipText}>Skip</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.artworkArea}>
-        <Image source={selected.image} resizeMode="contain" style={styles.artwork} />
-        <Text style={styles.text}>{selected.text}</Text>
-        <View style={styles.dotset}>
-          {
-            data.map(element => {
-              return (
-                <TouchableOpacity
-                  activeOpacity={1}
-                  onPress={() => {
-                    setData(element);
-                  }}
-                  style={[styles.dot, selected.id == element.id ? { backgroundColor: "#FF6EA1" } : { backgroundColor: "white" } ]}
-                ></TouchableOpacity>
-              )
-            })
-          }
 
-        </View>
-        {
-          selected.id == 3 ? (
-            <TouchableOpacity
-            activeOpacity={0.9}
-              style={styles.getStartedButton}
-            >
-              <Text style={styles.getStartedButtonText}>Get Started</Text>
-            </TouchableOpacity>
-          ) : null
-        }
-      </View>
     </View>
   );
 };
@@ -81,64 +43,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
-  topPanel: {
-    position: "absolute",
-    top: 55,
-    width: "100%",
-    height: 50,
-    justifyContent: "center",
-    alignItems: "flex-end",
-    paddingRight: 16,
-  },
-  skipText: {
-    color: '#969696',
-    fontSize: 17,
-  },
-  artworkArea: {
-    marginTop: 100,
-  },
-  artwork: {
-    width: 500,
-    height: 350,
-  },
-  text: {
-    marginTop: 50,
-    alignSelf: "center",
-    textAlign: "center",
-    fontSize: 23,
-    width: 300,
-    height: 60
-  },
-  dotset: {
-    marginTop: 30,
-    alignSelf: "center",
-    width: 100,
-    height: 30,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  dot: {
-    margin: 7,
-    width: 13,
-    height: 13,
-    borderRadius: 13,
-    borderWidth: 1.5,
-    borderColor: "#FF6EA1",
-    // backgroundColor: "#FF6EA1",
-  },
-  getStartedButton: {
-    alignSelf: "center",
-    marginTop: 15,
-    borderRadius: 7,
-    width: 210,
-    height: 50,
-    backgroundColor: "#FF6EA1",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  getStartedButtonText: {
-    color: "white",
-    fontSize: 21,
-  }
+
 });
